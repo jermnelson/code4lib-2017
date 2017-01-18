393eee160e18fc09e075fdce1cb22fed0a2ba8dc
Jeremy Nelson <jermnelson@gmail.com>
2017-01-18 16:54:19 -0700

    Added Content to base

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 app = Flask(__name__)
+
+@app.route("/<page>")
+def slide(page=None):
+    if not page:
+        redirect(home)
+    return render_template("{}.html".format(page))
 
 @app.route("/")
 def default():
     return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0", port=5005)
