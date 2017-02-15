2c2dd64ddfe01ce1c929343ebf53a185a5e1d4f0
Jeremy Nelson <jermnelson@gmail.com>
2017-02-15 14:22:56 -0700

    midday save point

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
 from flask import Flask, render_template, redirect
 app = Flask(__name__)
 
+#@app.template_filter('is_active')
+#def active_category(path):
+#    if path.endswith('catalog-pull-platform'):
+#        return 'active'
+#    elif path.endswith('setup-env') or path.endswith('create-knowledge-graph') or\
+#         path.endswith('ingesting-marc') or path.endswith('ingesting-metadata'):
+#        return 'active'
+#    elif path.endswith('cc-etd') or path.end
+
 @app.route("/<page>")
 def slide(page=None):
     if not page:
-        redirect(home)
+        redirect(default)
     return render_template("{}.html".format(page))
 
 @app.route("/")
