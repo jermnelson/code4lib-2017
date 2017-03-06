abf034a2c741f57a94b35a38e9c992acf1d8113b
Jeremy Nelson <jermnelson@gmail.com>
2017-03-06 09:26:18 -0800

    added example config.py file

---
+++ static/py/config.py
@@ -0,0 +1,27 @@
+DEFAULT_RDF_NS = {
+    "kds": "http://knowledgelinks.io/ns/data-structures/",
+    "kdr": "http://knowledgelinks.io/ns/data-resources/",
+    "bf": "http://id.loc.gov/ontologies/bibframe/",
+    "dpla": "http://dp.la/about/map/",
+    "skos": "http://www.w3.org/2004/02/skos/core#",
+    "loc": "http://id.loc.gov/authorities/",
+    "mods": "http://www.loc.gov/mods/v3",
+    "dc": "http://purl.org/dc/terms/",
+    "es": "http://knowledgelinks.io/ns/elasticsearch/",
+    "edm": "http://www.europeana.eu/schemas/edm/",
+    "schema": "http://schema.org/",
+    "xsd": "http://www.w3.org/2001/XMLSchema#",
+    "ore": "http://www.openarchives.org/ore/terms/",
+    "owl": "http://www.w3.org/2002/07/owl#",
+    "void": "http://rdfs.org/ns/void#",
+    "dcterm": "http://purl.org/dc/terms/",
+    "dc": "http://purl.org/dc/elements/1.1/",
+    "dbo": "http://dbpedia.org/ontology/",
+    "dbp": "http://dbpedia.org/property/",
+    "dbr": "http://dbpedia.org/resource/",
+    "m21": "<http://knowledgelinks.io/ns/marc21/>"
+}
+
+BASE_URL = 'http://bibcat.org/'
+SECRET_KEY ='b256efed34536fd67f48dbdfa192a7ea2fd553526afcfd1fcfebf7b21f2d1303'
+TRIPLESTORE_URL = 'http://localhost:9999/blazegraph/query' 
